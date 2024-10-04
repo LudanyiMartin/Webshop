1ab604dade4a3f99a1ea97f2adf54da310cd5653
Petrik Diak <diak@petrik.hu>
2024-10-04 16:35:28 +0200

    ell fele done

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { NewOrderDto } from './newOrder.dto';
+import { response } from 'express';
 
 @Controller()
 export class AppController {
@@ -11,5 +13,29 @@ export class AppController {
     return {
       message: this.appService.getHello()
     };
+  }
+
+  @Get('termekek')
+  @Render('termekek')
+  getTermek(){
+
+  }
+
+  @Post('termekek')
+  @Render('success')
+  newPurchase(
+    @Body() orderData: NewOrderDto,
+    @Res() response: Response
+  ) {
+    const errors: string [] = [];
+    if(!orderData.nev || !orderData.szaml_Orszag || !orderData.szaml_IranySzam || !orderData.szaml_Varos || !orderData.szaml_UtcaHsz || !orderData.szall_Orszag || !orderData.szall_IranySzam || !orderData.szall_Varos || !orderData.szall_UtcaHsz || !orderData.kartyaSzam || !orderData.lej_datum || !orderData.cvv || !orderData.kuponkod){
+      errors.push("Minden mezőt meg kell adni!👧🏿")
+    }
+    if(!/^\d{4}-\d{4}-\d{4}-\d{4}$/.test(orderData.kartyaSzam) || /^[A-Z]{2}-\d{4}$/.test(orderData.kuponkod)){
+      errors.push("A kártyaszám vagy a kuponkód helytelenül van megadva!👼🏿")
+    }
+    if(isNaN(orderData.szall_IranySzam) || isNaN(orderData.szaml_IranySzam) || isNaN(orderData.cvv)){
+
+    }
   }
 }
---
+++ src/newOrder.dto.ts
@@ -0,0 +1,16 @@
+export class NewOrderDto{
+  tej: string;
+  nev: string;
+  szaml_Orszag: string;
+  szaml_IranySzam: number;
+  szaml_Varos: string;
+  szaml_UtcaHsz: string;
+  szall_Orszag: string;
+  szall_IranySzam: number;
+  szall_Varos: string;
+  szall_UtcaHsz: string;
+  kartyaSzam: string;
+  lej_datum: Date;
+  cvv: number;
+  kuponkod: string;
+}
